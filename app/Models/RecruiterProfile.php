7ad813a815cd86ef45cab197684845f921cf727a
PK <pankajkachhwaye@gmail.com>
2017-08-26 17:34:55 +0530

    change in model

--- app/Models/RecruiterProfile.php
+++ app/Models/RecruiterProfile.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class RecruiterProfile extends Model
 {
     protected $table = 'recruiter_profile';
+
+    public function getOrgWebsiteAttribute($value)
+    {
+        return ($value == null ? '':$value);
+    }
+
+    public function getOrgLogoAttribute($value)
+    {
+        return ($value == null ? '':$value);
+    }
 }
